dbe7068e39cf42b85d661bf7897a650f3847563f
titrxw <1120309488@qq.com>
2020-11-18 17:07:46 +0800

    格式化文件

--- src/Queue/Connectors/CMQConnector.php
+++ src/Queue/Connectors/CMQConnector.php
@@ -7,35 +7,33 @@ use Freyo\LaravelQueueCMQ\Queue\Driver\Account;
 use Illuminate\Queue\Connectors\ConnectorInterface;
 use Illuminate\Support\Arr;
 
-class CMQConnector implements ConnectorInterface
-{
-    /**
-     * Establish a queue connection.
-     *
-     * @param array $config
-     *
-     * @throws \ReflectionException
-     *
-     * @return \Illuminate\Contracts\Queue\Queue
-     */
-    public function connect(array $config)
-    {
-        $queue = new Account(
-            Arr::get($config, 'options.queue.host'),
-            $config['secret_id'],
-            $config['secret_key']
-        );
+class CMQConnector implements ConnectorInterface {
+	/**
+	 * Establish a queue connection.
+	 *
+	 * @param array $config
+	 *
+	 * @throws \ReflectionException
+	 *
+	 * @return \Illuminate\Contracts\Queue\Queue
+	 */
+	public function connect(array $config) {
+		$queue = new Account(
+			Arr::get($config, 'options.queue.host'),
+			$config['secret_id'],
+			$config['secret_key']
+		);
 
-        if (Arr::get($config, 'options.topic.enable')) {
+		if (Arr::get($config, 'options.topic.enable')) {
 			$topic = new Account(
 				Arr::get($config, 'options.topic.host'),
 				$config['secret_id'],
 				$config['secret_key']
 			);
 		} else {
-        	$topic = null;
+			$topic = null;
 		}
 
-        return new CMQQueue($queue, $topic, $config);
-    }
+		return new CMQQueue($queue, $topic, $config);
+	}
 }
