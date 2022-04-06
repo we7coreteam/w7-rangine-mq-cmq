07c209641765c10054474ed7adfabf293a171aa2
titrxw <1120309488@qq.com>
2022-04-06 10:22:52 +0800

    fix: 修改消息尝试次数获取和修改

--- src/Queue/RabbitMqQueue.php
+++ src/Queue/RabbitMqQueue.php
@@ -1,13 +1,58 @@
 <?php
 
+/**
+ * WeEngine Api System
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
+ */
+
 namespace Freyo\LaravelQueueCMQ\Queue;
 
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Wire\AMQPTable;
 
 class RabbitMqQueue extends \W7\Mq\Queue\RabbitMQQueue {
-	public function laterRaw($delay, $payload, $queue = null, $attempts = 0)
-	{
+	/**
+	 * Create a AMQP message.
+	 *
+	 * @param $payload
+	 * @param int $attempts
+	 * @return array
+	 */
+	protected function createMessage($payload, int $attempts = 0): array {
+		$properties = [
+			'content_type' => 'application/json',
+			'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,
+		];
+
+		if ($correlationId = json_decode($payload, true)['id'] ?? null) {
+			$properties['correlation_id'] = $correlationId;
+		}
+
+		if ($this->isPrioritizeDelayed()) {
+			$properties['priority'] = $attempts;
+		}
+
+		$message = new AMQPMessage($payload, $properties);
+
+		$message->set('application_headers', new AMQPTable([
+			'laravel.attempts' => $attempts,
+			'laravel' => [
+				'attempts' => $attempts,
+			],
+		]));
+
+		return [
+			$message,
+			$correlationId,
+		];
+	}
+
+	public function laterRaw($delay, $payload, $queue = null, $attempts = 0) {
 		$ttl = $this->secondsUntil($delay) * 1000;
 
 		// When no ttl just publish a new message to the exchange or queue
@@ -16,7 +61,7 @@ class RabbitMqQueue extends \W7\Mq\Queue\RabbitMQQueue {
 		}
 
 		$queue = $this->getQueue($queue);
-		$destination = $queue.'.delay';
+		$destination = $queue . '.delay';
 
 		/**
 		 * @var AMQPMessage $message
